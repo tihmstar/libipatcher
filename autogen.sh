4f2133b92e4cfe1f3272be2124043f35c8051e38
tihmstar <tihmstar@gmail.com>
2017-06-07 16:30:20 +0200

    fixed versioning

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ autoheader
 automake --add-missing
 autoreconf -i
 sed -i "" -e "s/LIBRARY_GIT_VERSION_NUM/$(git rev-list --count HEAD)/g" configure.ac
-sed -i "" -e "s/LIBRARY_GIT_VERSION_NUM/$(git rev-list --count HEAD)/g" libipatcher.pc
+sed -i "" -e "s/LIBRARY_GIT_VERSION_NUM/$(git rev-list --count HEAD)/g" libipatcher.pc.in
 sed -i "" -e "s/LIBRARY_GIT_VERSION_NUM/$(git rev-list --count HEAD)/g" libipatcher/all_libipatcher.h
 sed -i "" -e "s/LIBRARY_GIT_VERSION_SHA/$(git rev-parse HEAD)/g" libipatcher/all_libipatcher.h
 ./configure "$@"
